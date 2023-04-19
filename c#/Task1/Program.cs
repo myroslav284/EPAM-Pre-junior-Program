cd9ca7ddac1c610d5f7f0a25333d46f903489394
myroslav284 <miroslav2267@gmail.com>
2023-04-19 21:58:19 +0300

    First task

--- c#/Task1/Program.cs
+++ c#/Task1/Program.cs
@@ -1,2 +1,17 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿Console.WriteLine("Pen price: ");
+
+double penPrice = Convert.ToDouble(Console.ReadLine());
+Console.WriteLine("Notebook price: ");
+
+double notebookPrice = Convert.ToDouble(Console.ReadLine());
+Console.WriteLine("Pens count: ");
+
+int penCount = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("Notebooks count: ");
+
+int notebookCount = Convert.ToInt32(Console.ReadLine());
+
+
+double sum =(penCount*penPrice)+ (notebookCount*notebookPrice);
+
+Console.WriteLine("All price: " + sum);
